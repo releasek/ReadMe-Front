c6d1ca65a6e6dc2be9c4fa1a39633305f7736500
white <a0983584929@gmail.com>
2025-01-05 15:26:06 +0800

    建立api

---
+++ ReadMe-Front/Models/Services/CartService.cs
@@ -0,0 +1,28 @@
+﻿using ReadMe_Front.Models.EFModels;
+using ReadMe_Front.Models.Repositories;
+using ReadMe_Front.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace ReadMe_Front.Models.Services
+{
+    public class CartService
+    {
+        private readonly CartEFRepo _cartEFRepo;
+        public CartService()
+        {
+            _cartEFRepo = new CartEFRepo();
+        }
+        public CartVm GetCartInfo(string account)
+        {
+            return _cartEFRepo.GetCartInfo(account);
+        }
+        public void AddCartItem(int cartId, int productId, int price)
+        {
+            _cartEFRepo.AddCartItem(cartId,productId,price);
+        }
+    }
+}
