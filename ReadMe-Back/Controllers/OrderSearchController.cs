0ec9dece61b36a22b692f2a00a43585a7cc33f3a
white <a0983584929@gmail.com>
2025-01-10 16:15:26 +0800

    訂單管理

---
+++ ReadMe-Back/Controllers/OrderSearchController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReadMe_Back.Models.EFModels;
+
+namespace ReadMe_Back.Controllers
+{
+    public class OrderSearchController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public OrderSearchController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OrderMain()
+        {
+            return View();
+        }
+    }
+}
