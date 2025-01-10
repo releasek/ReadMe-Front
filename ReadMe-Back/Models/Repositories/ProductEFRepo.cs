8cafff624c8b7851b27c6b4daa87a0ae34a93922
Abner <abneryps@gmail.com>
2025-01-10 14:37:12 +0800

    寫 product repo中

--- ReadMe-Back/Models/Repositories/ProductEFRepo.cs
+++ ReadMe-Back/Models/Repositories/ProductEFRepo.cs
@@ -1,6 +1,17 @@
-﻿namespace ReadMe_Back.Models.Repositories
+﻿using ReadMe_Back.Models.EFModels;
+
+namespace ReadMe_Back.Models.Repositories
 {
     public class ProductEFRepo
     {
+        private readonly AppDbContext _db;
+        public ProductEFRepo(AppDbContext db)
+        {
+            this._db = db;
+        }
+
+        public void GetProducts()
+        {
+        }
     }
 }
