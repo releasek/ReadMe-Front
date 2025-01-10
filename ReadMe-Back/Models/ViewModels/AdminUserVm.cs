1360bdf94fc303a1e12877f6bf3294565b1c745a
Zhu <suing0712@gmail.com>
2025-01-10 17:36:11 +0800

    Admin view

---
+++ ReadMe-Back/Models/ViewModels/AdminUserVm.cs
@@ -0,0 +1,15 @@
+﻿namespace ReadMe_Back.Models.ViewModels
+{
+    public class AdminUserVm
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string GroupName { get; set; }
+
+        //public string RoleName { get; set; }
+
+        public string PermissionName { get; set; }
+    }
+}
